2a07300e06c5f89bde73a971ef2b513b38b3de8b
sinilandia <aisinilandia@gmail.com>
2019-03-22 21:14:22 +0200

    lisää MaksukorttiTest Unicafelle

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -4,17 +4,67 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+//saldo vähenee oikein, jos rahaa on tarpeeksi
+//saldo ei muutu, jos rahaa ei ole tarpeeksi
+//metodi palauttaa true, jos rahat riittivät ja muuten false
+
 public class MaksukorttiTest {
 
     Maksukortti kortti;
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(500);
+        assertEquals(1500, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanOttaminenToimii() {
+        kortti.otaRahaa(500);
+        assertEquals(500, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi() {
+        kortti.otaRahaa(1000);
+        assertEquals(0, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(5000);
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test
+    public void metodiPalauttaaTrueJosRahatRiittivat() {
+       assertTrue(kortti.otaRahaa(500));
+    }
+   
+    @Test
+    public void metodiPalauttaaFalseJosRahatEivatRiita() {
+       assertFalse(kortti.otaRahaa(5000));
+    }
+    
+    @Test
+    public void toStringToimiiOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    
 }
